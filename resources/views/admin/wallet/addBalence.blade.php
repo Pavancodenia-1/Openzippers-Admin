cb07a32f79e89a11f88f1b5ed85f7cafd55022f7
Pavancodenia-1 <pavan.kumar@corewebconnections.com>
2025-03-20 15:23:56 +0530

    fix: remove extraneous text from addBalance view

--- resources/views/admin/wallet/addBalence.blade.php
+++ resources/views/admin/wallet/addBalence.blade.php
@@ -95,5 +95,5 @@ $useroption[$usr->id] = $usr->name . ' , ' . $usr->email;
             $('.product-select').select2(); 
         });
 </script> -->
-<!-- use for multiple dropdowns -->
+<!-- use for multiple dropdowns -->hello
 @endsection
